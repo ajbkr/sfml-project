02917a3c3050321f1e26437187fd16ea6298b91c
Andrew Baker <andrew_j_baker2@hotmail.com>
2021-06-12 18:49:23 +0100

    Time

--- main.cpp
+++ main.cpp
@@ -11,48 +11,24 @@ int main() {
 
   sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
 
-  sf::CircleShape shape(100.0F);
-  shape.setFillColor(sf::Color::Green);
-
-  int x = 0, y = 0;
+  sf::Clock clock;
+  clock.restart();
+  sf::Time time;
 
   while (window.isOpen()) {
+    time = clock.getElapsedTime();
+    float seconds = time.asSeconds();
+    std::cout << seconds << std::endl;
+
     sf::Event event;
 
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed) {
         window.close();
-      } else if (event.type == sf::Event::MouseButtonPressed) {
-        std::cout << "Mouse button pressed" << std::endl;
-      } else if (event.type == sf::Event::KeyPressed) {
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-          std::cout << "Space bar pressed" << std::endl;
-        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-          if (x >= 10) {
-            x -= 10;
-          }
-          shape.setPosition(x, y);
-        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-          if (x <= 290) {
-            x += 10;
-          }
-          shape.setPosition(x, y);
-        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-          if (y >= 10) {
-            y -= 10;
-          }
-          shape.setPosition(x, y);
-        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-          if (y <= 290) {
-            y += 10;
-          }
-          shape.setPosition(x, y);
-        }
       }
     }
 
     window.clear();
-    window.draw(shape);
     window.display();
   }
 
